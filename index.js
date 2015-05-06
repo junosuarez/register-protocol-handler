08e5517b24d89c880d3d35afe7ae293cc4b71aa6
jden <jason@denizac.org>
2015-05-05 20:30:36 -0700

    switch on platform

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function registerProtocolHandler () {
-  
+var registerProtocolHandler
+
+switch (process.platform) {
+  case 'win32':
+    registerProtocolHandler = require('register-protocol-handler-windows')
+    break;
+  default:
+    throw new Error(process.platform + ' not yet supported - PRs welcome! https://github.com/jden/register-protocol-handler')
 }
 
 module.exports = registerProtocolHandler
